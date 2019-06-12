188e98032abd8cb8039185dbe7a0dfe2c65a1d7b
goaty <bone_hacker@hotmail.com>
2019-06-12 18:56:22 +0200

    Folder reorganization + finished Navigation

---
+++ Sedc.AttributeRouting.WebApp/Controllers/PizzaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sedc.AttributeRouting.WebApp.Controllers
+{
+    [Route("pizzas")]
+    public class PizzaController : Controller
+    {
+        [Route("menu", Name = "menu")]
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        [Route("offers", Name = "offers")]
+        public IActionResult Offers()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sedc.ConventionalRouting.WebApp/Controllers/PizzaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sedc.ConventionalRouting.WebApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        public IActionResult Offers()
+        {
+            return View();
+        }
+    }
+}
