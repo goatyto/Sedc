39d181a73c268c9d7a6de6e93811a54a09015197
goaty <bone_hacker@hotmail.com>
2019-06-13 13:24:45 +0200

    Initial version of "UI&Forms" examples

---
+++ Sedc.FormValidation.WebApp/Controllers/PizzaController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Sedc.FormValidation.WebApp.Models;
+
+namespace Sedc.FormValidation.WebApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private SelectList PizzaStyles { get; set; }
+
+        public PizzaController()
+        {
+            PizzaStyles = new SelectList(new []
+            {
+                "Neapolitan",
+                "New Heaven",
+                "Grandma Style",
+                "New York Style",
+                "Pan Style",
+            });
+        }
+
+        public IActionResult AddPizza()
+        {
+            ViewBag.PizzaStyles = PizzaStyles;
+
+            return View(new Pizza());
+        }
+
+        [HttpPost]
+        public IActionResult AddPizza(Pizza pizza)
+        {
+            ViewBag.PizzaStyles = PizzaStyles;
+
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = $"Successfully added {pizza.Name}";
+                return View(new Pizza());
+            }
+
+            return View(pizza);
+        }
+    }
+}
---
+++ Sedc.SimpleForm.WebApp/Controllers/PizzaController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Sedc.SimpleForm.WebApp.Models;
+
+namespace Sedc.SimpleForm.WebApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private SelectList PizzaStyles { get; set; }
+
+        public PizzaController()
+        {
+            PizzaStyles = new SelectList(new []
+            {
+                "Neapolitan",
+                "New Heaven",
+                "Grandma Style",
+                "New York Style",
+                "Pan Style",
+            });
+        }
+
+        public IActionResult AddPizza()
+        {
+            ViewBag.PizzaStyles = PizzaStyles;
+
+            return View(new Pizza());
+        }
+
+        [HttpPost]
+        public IActionResult AddPizza(Pizza pizza)
+        {
+            ViewBag.PizzaStyles = PizzaStyles;
+            ViewBag.Message = $"Successfully added {pizza.Name}";
+
+            return View(new Pizza());
+        }
+    }
+}
