e76aa41676f8a71641d63ee1c950e9f05e48db0e
goaty <bone_hacker@hotmail.com>
2019-06-13 15:20:35 +0200

    Various changes + initial version of Filters example

--- Sedc.FormValidation.WebApp/Controllers/PizzaController.cs
+++ Sedc.FormValidation.WebApp/Controllers/PizzaController.cs
@@ -6,32 +6,14 @@ namespace Sedc.FormValidation.WebApp.Controllers
 {
     public class PizzaController : Controller
     {
-        private SelectList PizzaStyles { get; set; }
-
-        public PizzaController()
-        {
-            PizzaStyles = new SelectList(new []
-            {
-                "Neapolitan",
-                "New Heaven",
-                "Grandma Style",
-                "New York Style",
-                "Pan Style",
-            });
-        }
-
         public IActionResult AddPizza()
         {
-            ViewBag.PizzaStyles = PizzaStyles;
-
             return View(new Pizza());
         }
 
         [HttpPost]
         public IActionResult AddPizza(Pizza pizza)
         {
-            ViewBag.PizzaStyles = PizzaStyles;
-
             if (ModelState.IsValid)
             {
                 ViewBag.Message = $"Successfully added {pizza.Name}";
--- Sedc.SimpleForm.WebApp/Controllers/PizzaController.cs
+++ Sedc.SimpleForm.WebApp/Controllers/PizzaController.cs
@@ -10,31 +10,14 @@ namespace Sedc.SimpleForm.WebApp.Controllers
 {
     public class PizzaController : Controller
     {
-        private SelectList PizzaStyles { get; set; }
-
-        public PizzaController()
-        {
-            PizzaStyles = new SelectList(new []
-            {
-                "Neapolitan",
-                "New Heaven",
-                "Grandma Style",
-                "New York Style",
-                "Pan Style",
-            });
-        }
-
         public IActionResult AddPizza()
         {
-            ViewBag.PizzaStyles = PizzaStyles;
-
             return View(new Pizza());
         }
 
         [HttpPost]
         public IActionResult AddPizza(Pizza pizza)
         {
-            ViewBag.PizzaStyles = PizzaStyles;
             ViewBag.Message = $"Successfully added {pizza.Name}";
 
             return View(new Pizza());
